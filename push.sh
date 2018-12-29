aa56866435c24606de7f39e2bd8e47c91e68ef7e
axier <aartola1986@gmail.com>
2018-12-29 17:21:19 +0100

    login docker hub

--- push.sh
+++ push.sh
@@ -8,6 +8,10 @@ IMAGE=java
 version=`cat VERSION`
 buildDate=`date +"%y.%m.%d"`
 
+# login dockerhub
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
